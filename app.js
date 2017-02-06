e8a76cdf9f5f035ecc462b8296a31bd82b7978fc
timocodex <timothy.og.siahaan@gmail.com>
2017-02-07 06:48:06 +0700

    done copy twatt

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const router = express.Router()
+const config = require('./config.js')
+const Twit = require('twit')
+const T = new Twit({
+  consumer_key:         config.consumer_key,
+  consumer_secret:      config.consumer_secret,
+  access_token:         config.access_token,
+  access_token_secret:  config.access_token_secret,
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+})
+
+router.get('/newTweet',function(req,res){
+  T.get('statuses/user_timeline', function(err, data, response) {
+    res.send(data)
+  })
+})
+
+app.use('/',router)
+app.listen(3000)
