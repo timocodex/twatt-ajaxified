b70b0cb3c42e395b601d8dd7d9cb7339e1b639fa
timocodex <timothy.og.siahaan@gmail.com>
2017-02-07 07:55:44 +0700

    success show recent

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express()
 const router = express.Router()
 const config = require('./config.js')
 const Twit = require('twit')
+const cors = require('cors')
+
 const T = new Twit({
   consumer_key:         config.consumer_key,
   consumer_secret:      config.consumer_secret,
@@ -11,11 +13,19 @@ const T = new Twit({
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
 
-router.get('/newTweet',function(req,res){
+router.get('/myTweet',function(req,res){
   T.get('statuses/user_timeline', function(err, data, response) {
     res.send(data)
   })
 })
 
+router.post('/tweetPost',function(req,res){
+
+  T.post('statuses/update',{status:req.body.tweet}, function(err, data, response) {
+    res.send(data)
+  })
+})
+
+app.use(cors());
 app.use('/',router)
 app.listen(3000)
